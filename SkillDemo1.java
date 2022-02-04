f92aaaed7a00df4c15d3467362a0c83856f45211
tylermeyers <88351548+tylermeyers@users.noreply.github.com>
2022-02-04 15:47:54 -0800

    fail test

--- SkillDemo1.java
+++ SkillDemo1.java
@@ -1,1 +1,5 @@
-
+public class SkillDemo1{
+    public static int minus(int n1, int n2 ){
+        return (n1-n2);
+    }
+}
---
+++ SkillDemo1Test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemo1Test {
+@Test
+    public void minus(){
+        assertEquals(2,SkillDemo1.minus(4,2));
+    }
+}
